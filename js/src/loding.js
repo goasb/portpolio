2409339e30b7b5e222200afb2fb4ab94d8cf8763
chojaeyeong <goasb18@gmail.com>
2019-12-12 18:12:04 +0900

    portfolio

---
+++ js/src/loding.js
@@ -0,0 +1,11 @@
+(function($){
+    const active = $('.img');
+    setTimeout(function(){
+        active.addClass('active');
+    },700);
+
+setTimeout(function(){
+    window.location = "./main.html";
+},3000);
+
+})(jQuery);
