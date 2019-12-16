1cf0d4b110ce694839e33c72146503346d703dde
chojaeyeong <goasb18@gmail.com>
2019-12-16 16:30:54 +0900

    0430

---
+++ js/src/main.js
@@ -0,0 +1,14 @@
+(function($){
+ const moniter = $('.moniter');
+ const portImg = $('.port_img');
+
+//  moniter.on('mouseenter',function(){
+//     portImg.stop().animate({top: '-635px'},2000)
+//  });
+
+//  moniter.on('mouseleave',function(){
+//     portImg.stop().animate({top: 0},2000)
+//  });
+
+
+})(jQuery);
