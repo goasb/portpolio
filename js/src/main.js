a797b40bba261b7677ba571c84b7763b6fe6e9c8
chojaeyeong <goasb18@gmail.com>
2019-12-16 17:59:42 +0900

    20191216

--- js/src/main.js
+++ js/src/main.js
@@ -1,14 +1,78 @@
 (function($){
- const moniter = $('.moniter');
- const portImg = $('.port_img');
 
-//  moniter.on('mouseenter',function(){
-//     portImg.stop().animate({top: '-635px'},2000)
-//  });
+ const htmlEl = $('html,body');
+ const wrap = $('#wrap');
+ const scrollEl = wrap.find('.scroll');
+ const gnb = $('.gnb');
+ const gnbUl = gnb.find('ul');
+ const gnbLi = gnbUl.find('li');
+ const gnbLink = gnbLi.find('a');
 
-//  moniter.on('mouseleave',function(){
-//     portImg.stop().animate({top: 0},2000)
-//  });
+//  마우스 휠 ===================================
+ htmlEl.animate({scrollTop:0});
+ let myScrollElTop = [];
+ let scrollLen = scrollEl.length;
+ let timed = 500;
 
+ for(let i = 0; i < scrollLen; i++){
+    let scTop = scrollEl.eq(i).offset().top;
+    myScrollElTop.push(scTop);
+ }
+
+ let myStatus = true , n , useN = 0;
+
+ const ScrollMagic = function(){
+     htmlEl.animate({scrollTop:myScrollElTop[useN]},
+        timed,'easeOutSine',function(){
+            myStatus = true;
+   });
+ }; // scrollMagic()
+
+ $(window).on('mousewheel DOMMouseScroll',function(e){
+     if(e.originalEvent.wheelDelta){
+         n = e.originalEvent.wheelDelta * -1;
+     }else{n = e.originalEvent.delta * 40;}
+     
+     if(myStatus){
+         myStatus = false;
+       if(n > 0){useN++;
+        if(useN >= scrollLen){useN = scrollLen-1;} ScrollMagic();
+      }else{useN--;
+        if(useN < 0){useN = 0;} ScrollMagic();}
+     }
+ });
+ //  마우스 휠 ===================================
+    gnbLink.on('click',function(e){
+        e.preventDefault();
+        useN = $(this).parent('li').index();
+        ScrollMasic();
+    });
+// ==============================================
+// 터치 사용시
+   let startP,endP;
+   $(window).on('touchstart',function(e){
+    startP = e.originalEvent.touches[0].pageY;
+   }); // touchstart
+
+   $(window).on('touchmove',function(){
+    htmlEl.animate({scrollTop:myScrollElTop[useN]},0);
+   }); // touchmove
+
+   $(window).on('touchend',function(e){
+       endP = e.originalEvent.changedTouches[0].pageY;
+      
+    if(myStatus){
+        myStatus = false;
+        if(startP > endP){
+            useN++;
+            if(useN >= scrollLen){useN = scrollLen-1;}
+            ScrollMagic();
+        }else{
+            useN--;
+            if(useN < 0){useN = 0;}
+            ScrollMagic();
+        }
+    }
+   });
 
 })(jQuery);
