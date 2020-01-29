1b3bdc92d4509e1776eb4dee649510e50d8154b1
chojaeyeong <you@example.com>
2020-01-29 18:56:07 +0900

    20200129

--- js/src/motion.js
+++ js/src/motion.js
@@ -3,6 +3,7 @@
 (function($){
     const motion = $('.motion_dell');
     const motionT = $('.motion_tesla');
+    const motionN = $('.motion_nivea');
     const active = motion.find('.span');
     
     // setTimeout(function(){
@@ -21,6 +22,14 @@
     });
     motionT.find('a').on('mouseleave',function(){
         motionT.removeClass('active');
+    });
+
+    
+    motionN.find('a').on('mouseenter',function(){
+        motionN.addClass('active');
+    });
+    motionN.find('a').on('mouseleave',function(){
+        motionN.removeClass('active');
     });
 
 })(jQuery);
